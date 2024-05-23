57fa356033e91e102fee93f7f8aefd28976a379f
Yoon <kohyc97@gmail.com>
2024-05-23 16:03:23 +0900

    first commit

---
+++ recipe_of_five_front/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import SearchInd from "./components/SearchInd";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+    return (
+        <div className="App">
+            <SearchInd />
+        </div>
+    );
+}
+
+export default App;
