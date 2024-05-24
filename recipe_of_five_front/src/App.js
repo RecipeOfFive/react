d2ed6d1374a92e516adc272c9a448c1cba8f114c
Hoik.jang <wkdghdlr1@naver.com>
2024-05-24 15:31:07 +0900

    Ranking basic css is finished

--- recipe_of_five_front/src/App.js
+++ recipe_of_five_front/src/App.js
@@ -1,10 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import SearchInd from "./components/SearchInd";
-import Ranking from "./components/Ranking";
+import Ranking from "./components/Ranking/Ranking";
 function App() {
     return (
         <div className="App">
-            {/* <SearchInd /> */}
+            <SearchInd />
             <Ranking />
         </div>
     );
---
+++ recipe_of_five_front/src/components/Ranking/Ranking.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import "./style.css";
+export default function Ranking() {
+  return (
+    <Card style={{ width: "50rem" }}>
+      <Card.Body className="Rank-location">
+        <div>
+          <Card.Subtitle className="mb-2 text-muted">
+            뒤틀린 황천의 오리 발바닥
+          </Card.Subtitle>
+          <Card.Text>
+            "뒤틀린 황천의 오리 발바닥"은 흔히 '먼 곳', '외딴 곳', '정서적으로
+            먼 곳'을 의미합니다. 이 표현은 상황이나 장소가 매우 멀고 외딴 곳에
+            있거나, 이해하기 어려운 상황을 나타낼 때 사용됩니다. 이 표현은 주로
+            비유적으로 사용되며, 어떤 것이나 어떤 상황이 이해하기 어렵거나
+            해결하기 어려운 정도를 강조할 때 사용됩니다.
+          </Card.Text>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </div>
+        <div>
+          <Card.Img
+            variant="top"
+            src="https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202312/17/f2cf3672-26b0-4613-82e0-8a0133986726.jpg"
+          />
+        </div>
+      </Card.Body>
+    </Card>
+  );
+}
