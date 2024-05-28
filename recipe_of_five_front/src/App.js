ec063ee1bca8dcfc62e464f0c1d8ca60c03556db
do-yoongyo2 <do.yoongyo2@gmail.com>
2024-05-28 15:26:24 +0900

    style: 화면 분할 추가

--- recipe_of_five_front/src/App.js
+++ recipe_of_five_front/src/App.js
@@ -1,39 +1,42 @@
-
 import React, { createContext } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 import SearchInd from "./components/SearchInd/SearchInd";
 import Ranking from "./components/Ranking/Ranking";
-import RecipeDescrip from "./components/RecipeDesc/RecipeDescrip"
+import RecipeDescrip from "./components/RecipeDesc/RecipeDescrip";
 import FilterBtn from "./components/SearchInd/FilterBtn";
 import SearchProvider from "./context/SearchProvider";
 import Ingredients from "./components/SearchInd/Ingredients";
 import SendSearch from "./components/SearchInd/SendSearch";
 import RecipeResult from "./components/RecipeResult/RecipeResult";
 
-
 export const AppContext = createContext();
 
 function App() {
-    return (
-        <>
-            <SearchProvider>
-                <div className="App">
-
-                    <SearchInd />
-                    <FilterBtn />
-                    <Ingredients />
-
-                    <RecipeResult />
-                    <SendSearch />
-
-
-                    <Ranking />
-                </div>
-            </SearchProvider>
-        </>
-
-    );
+  return (
+    <>
+      <SearchProvider>
+        <div className="App">
+          <Container fluid>
+            <Row>
+              <Col xs={6} md={6}>
+                <SearchInd />
+                <FilterBtn />
+                <Ingredients />
+                <SendSearch />
+              </Col>
+              <Col xs={6} md={6}>
+                {/* (검색이 실행함 -> true/false ? <검색 결과 /> : <Ranking /> ) */}
+                <RecipeResult />
+                <Ranking />
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </SearchProvider>
+    </>
+  );
 }
 
 export default App;
