ae074ca881a3a7339df3225bf76e3759b97a1102
Yoon <kohyc97@gmail.com>
2024-05-29 08:19:38 +0900

    feat: 메뉴 카드 받아오기

--- recipe_of_five_front/src/components/RecipeCard/RecipeCard.jsx
+++ recipe_of_five_front/src/components/RecipeCard/RecipeCard.jsx
@@ -1,6 +1,47 @@
-import React from "react";
-import { RecipeFilterContext } from "../context/SearchProvider";
+import React, { useContext } from "react";
+import { Card, ListGroup } from "react-bootstrap";
+import { RecipeFilterContext } from "../../context/SearchProvider";
+import "./style.css";
 
 export default function RecipeCard() {
-  return <div>RecipeCard</div>;
+    const { searchResult } = useContext(RecipeFilterContext);
+    return (
+        <div>
+            <div>
+                <Card style={{ width: "18rem" }}>
+                    {/* <Card.Img variant="top" src="holder.js/100px180?text=Image cap" /> */}
+
+                    <img src="https://recipe1.ezmember.co.kr/cache/recipe/2023/05/16/2b22d07bbb6ac9d0ee4c0438691819161.jpg"></img>
+                    <ListGroup className="list-group-flush">
+                        <Card.Title>Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title
+                            and make up the bulk of the card's content.
+                        </Card.Text>
+                    </ListGroup>
+                    <Card.Body className="text-align">
+                        <Card.Link href="#">Like : 5</Card.Link>
+                        <Card.Link href="#">ViewCount : 4</Card.Link>
+                    </Card.Body>
+                </Card>
+            </div>
+
+            {searchResult.map((el, index) => {
+                if (index === 10) return;
+                return (
+                    <Card key={index} style={{ width: "18rem" }}>
+                        <img src={el.main_image}></img>
+                        <ListGroup className="list-group-flush">
+                            <Card.Title>{el.name}</Card.Title>
+                            <Card.Text>{el.description}</Card.Text>
+                        </ListGroup>
+                        <Card.Body className="text-align">
+                            <Card.Link>Like : {el.likeCount}</Card.Link>
+                            <Card.Link>ViewCount : {el.view_count}</Card.Link>
+                        </Card.Body>
+                    </Card>
+                );
+            })}
+        </div>
+    );
 }
--- recipe_of_five_front/src/components/SearchInd/SendSearch.jsx
+++ recipe_of_five_front/src/components/SearchInd/SendSearch.jsx
@@ -4,52 +4,54 @@ import { RecipeFilterContext } from "../../context/SearchProvider";
 import axios from "axios";
 
 export default function SendSearch() {
-  const {
-    searchOptions,
-    setSearchOptions,
-    filterMethod,
-    include,
-    exclude,
-    ranking,
-    setRanking,
-  } = useContext(RecipeFilterContext);
+    const {
+        searchOptions,
+        setSearchOptions,
+        filterMethod,
+        include,
+        exclude,
+        setRanking,
+        searchResult,
+        filterRecipe,
+    } = useContext(RecipeFilterContext);
 
-  const handleSearch = () => {
-    setSearchOptions({
-      order: "like_count",
-      include: include,
-      exclude: exclude,
-      type: filterMethod,
-    });
-    //버튼 클릭시 세부 홈페이지로 이동 예정
-    //navigate("/detail");
-  };
+    const handleSearch = () => {
+        setSearchOptions({
+            order: "like_count",
+            include: include,
+            exclude: exclude,
+            type: filterMethod,
+        });
+        //버튼 클릭시 세부 홈페이지로 이동 예정
+        //navigate("/detail");
+    };
 
-  // 최초 랭킹 화면 get 요청
-  useEffect(() => {
-    axios
-      .post(
-        // 임시 url
-        "http://ec2-3-38-45-40.ap-northeast-2.compute.amazonaws.com:3000/api/food/",
-        searchOptions
-      )
-      .then((resp) => {
-        setRanking(resp.data);
-      });
-  }, []);
+    // 최초 랭킹 화면 get 요청
+    useEffect(() => {
+        axios
+            .post(
+                // 임시 url
+                "http://ec2-3-38-45-40.ap-northeast-2.compute.amazonaws.com:3000/api/food/",
+                searchOptions
+            )
+            .then((resp) => {
+                setRanking(resp.data);
+            });
+    }, []);
 
-  useEffect(() => {
-    // 서버에 원하는 레시피를 전송
-    // axios.get("url",searchOptions).then((resp) => {
-    //     이 후 정보 처리;
-    // });
-  }, [searchOptions]);
+    useEffect(() => {
+        //searchResult 결과
+        filterRecipe();
+    }, [searchOptions]);
 
-  return (
-    <div>
-      <Button variant="danger" onClick={handleSearch}>
-        검색
-      </Button>
-    </div>
-  );
+    console.log("요청 :", searchOptions);
+    console.log("결과 :", searchResult);
+
+    return (
+        <div>
+            <Button variant="danger" onClick={handleSearch}>
+                검색
+            </Button>
+        </div>
+    );
 }
