c1139efd36f00b84ed577c72c52ed58935222273
sktmdgus1212 <sktmdgus1212@naver.com>
2024-05-28 09:15:30 +0900

    fix: add axios library in context

--- recipe_of_five_front/src/context/SearchProvider.jsx
+++ recipe_of_five_front/src/context/SearchProvider.jsx
@@ -1,52 +1,53 @@
+import axios from "axios";
 import React, { createContext, useCallback, useEffect } from "react";
 import { useState } from "react";
 
 export const RecipeFilterContext = createContext();
 
 export default function SearchProvider({ children }) {
-    const [searchOptions, setSearchOptions] = useState({
-        include: [],
-        exclude: [],
-        method: [],
-    });
+  const [searchOptions, setSearchOptions] = useState({
+    include: [],
+    exclude: [],
+    method: [],
+  });
 
-    //초기 ranking
-    const [ranking, setRanking] = useState([]);
+  //초기 ranking
+  const [ranking, setRanking] = useState([]);
 
-    const [filterMethod, setFilterMethod] = useState([]);
-    const [include, setInclude] = useState([]);
-    const [exclude, setExclude] = useState([]);
-    const [order, setOrder] = useState("LIKECOUNT");
+  const [filterMethod, setFilterMethod] = useState([]);
+  const [include, setInclude] = useState([]);
+  const [exclude, setExclude] = useState([]);
+  const [order, setOrder] = useState("LIKECOUNT");
 
-    const [searchResult, setSearchResult] = [];
+  const [searchResult, setSearchResult] = [];
 
-    const filterRecipe = () => {
-        axios
-            .get("url", {
-                params: searchOptions,
-            })
-            .then((resp) => {
-                setSearchResult(resp.data);
-            });
-    };
+  const filterRecipe = () => {
+    axios
+      .get("url", {
+        params: searchOptions,
+      })
+      .then((resp) => {
+        setSearchResult(resp.data);
+      });
+  };
 
-    //value에 추후 state 추가
-    return (
-        <RecipeFilterContext.Provider
-            value={{
-                searchOptions,
-                setSearchOptions,
-                filterMethod,
-                setFilterMethod,
-                include,
-                setInclude,
-                exclude,
-                setExclude,
-                ranking,
-                setRanking,
-            }}
-        >
-            {children}
-        </RecipeFilterContext.Provider>
-    );
+  //value에 추후 state 추가
+  return (
+    <RecipeFilterContext.Provider
+      value={{
+        searchOptions,
+        setSearchOptions,
+        filterMethod,
+        setFilterMethod,
+        include,
+        setInclude,
+        exclude,
+        setExclude,
+        ranking,
+        setRanking,
+      }}
+    >
+      {children}
+    </RecipeFilterContext.Provider>
+  );
 }
