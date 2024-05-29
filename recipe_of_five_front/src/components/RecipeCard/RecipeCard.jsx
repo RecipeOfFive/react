fb4869b5606d54a403c4068005be24ae06ea3f8d
Yoon <kohyc97@gmail.com>
2024-05-29 09:06:50 +0900

    feat: 카드 좋아요 증가 요청 추가

--- recipe_of_five_front/src/components/RecipeCard/RecipeCard.jsx
+++ recipe_of_five_front/src/components/RecipeCard/RecipeCard.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Card, ListGroup, Button } from "react-bootstrap";
 import { RecipeFilterContext } from "../../context/SearchProvider";
 import "./style.css";
+import axios from "axios";
 
 export default function RecipeCard() {
     const { searchResult } = useContext(RecipeFilterContext);
@@ -36,7 +37,16 @@ export default function RecipeCard() {
                             <Card.Text>{el.description}</Card.Text>
                         </ListGroup>
                         <Card.Body className="text-align">
-                            <Button variant="primary">좋아요</Button>
+                            <Button
+                                variant="primary"
+                                onClick={() => {
+                                    axios.get(
+                                        `http://ec2-3-38-45-40.ap-northeast-2.compute.amazonaws.com:3000/api/food/like/${el.id}`
+                                    );
+                                }}
+                            >
+                                좋아요
+                            </Button>
                             {el.like_count}
                             <br />
                             <p>조회수 : {el.view_count}</p>
