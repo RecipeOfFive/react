556dec6e4e8f2fcc007bd7a90163e4f3f1ad6564
do-yoongyo2 <do.yoongyo2@gmail.com>
2024-05-29 15:19:14 +0900

    style: 토글 버튼 가운데 정렬 적용

--- recipe_of_five_front/src/components/SearchInd/SearchInd.jsx
+++ recipe_of_five_front/src/components/SearchInd/SearchInd.jsx
@@ -93,57 +93,61 @@ export default function SearchInd() {
   return (
     <div className="main-div-SearchInd">
       <h1>Recipe of Five</h1>
-      <ButtonGroup className="main-div-SearchInd-buttongroup">
-        {radios.map((radio, idx) => (
-          <ToggleButton
-            className={`main-div-SearchInd-togglebtn ${
-              radioValue === radio.value ? "custom-active" : "custom-inactive"
-            }`}
-            key={idx}
-            id={`radio-${idx}`}
-            type="radio"
+      <div className="searchInd-btn">
+        <ButtonGroup className="main-div-SearchInd-buttongroup">
+          {radios.map((radio, idx) => (
+            <ToggleButton
+              className={`main-div-SearchInd-togglebtn ${
+                radioValue === radio.value ? "custom-active" : "custom-inactive"
+              }`}
+              key={idx}
+              id={`radio-${idx}`}
+              type="radio"
+              variant="outline-secondary"
+              name="radio"
+              value={radio.value}
+              checked={radioValue === radio.value}
+              onChange={(e) => {
+                setRadioValue(e.currentTarget.value);
+                setIsInclude(radio.value === "1");
+              }}
+            >
+              {radio.name}
+            </ToggleButton>
+          ))}
+        </ButtonGroup>
+      </div>
+      <div className="searchInd-input">
+        <InputGroup className="mb-3">
+          <Form.Control
+            value={input}
+            placeholder="검색할 재료를 입력하세요"
+            aria-label="Recipient's username"
+            aria-describedby="basic-addon2"
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <Button
             variant="outline-secondary"
-            name="radio"
-            value={radio.value}
-            checked={radioValue === radio.value}
-            onChange={(e) => {
-              setRadioValue(e.currentTarget.value);
-              setIsInclude(radio.value === "1");
+            id="button-addon2"
+            onClick={() => {
+              if (include.includes(input)) {
+                alert("이미 포함된 재료입니다.");
+                return;
+              } else if (exclude.includes(input)) {
+                alert("이미 제외된 재료입니다.");
+                return;
+              } else if (isInclude) {
+                setInclude((prev) => [...prev, input]);
+              } else {
+                setExclude((prev) => [...prev, input]);
+              }
+              setInput("");
             }}
           >
-            {radio.name}
-          </ToggleButton>
-        ))}
-      </ButtonGroup>
-      <InputGroup className="mb-3">
-        <Form.Control
-          value={input}
-          placeholder="검색할 재료를 입력하세요"
-          aria-label="Recipient's username"
-          aria-describedby="basic-addon2"
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <Button
-          variant="outline-secondary"
-          id="button-addon2"
-          onClick={() => {
-            if (include.includes(input)) {
-              alert("이미 포함된 재료입니다.");
-              return;
-            } else if (exclude.includes(input)) {
-              alert("이미 제외된 재료입니다.");
-              return;
-            } else if (isInclude) {
-              setInclude((prev) => [...prev, input]);
-            } else {
-              setExclude((prev) => [...prev, input]);
-            }
-            setInput("");
-          }}
-        >
-          적용
-        </Button>
-      </InputGroup>
+            적용
+          </Button>
+        </InputGroup>
+      </div>
     </div>
   );
 }
