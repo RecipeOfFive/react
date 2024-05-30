95f6cc8a5a7dd4e4fec97469280a441feed948a8
Yoon <kohyc97@gmail.com>
2024-05-30 13:54:37 +0900

    feat: 설정된 재료창 변경

--- recipe_of_five_front/src/components/SearchInd/Ingredients.jsx
+++ recipe_of_five_front/src/components/SearchInd/Ingredients.jsx
@@ -1,44 +1,52 @@
 import React, { useContext, useState, useEffect } from "react";
 import { RecipeFilterContext } from "../../context/SearchProvider";
+import { Button } from "react-bootstrap";
 import "./style.css";
 
 export default function SearchInd() {
-  const { exclude, setExclude, include, setInclude } =
-    useContext(RecipeFilterContext);
+    const { exclude, setExclude, include, setInclude } =
+        useContext(RecipeFilterContext);
 
-  return (
-    <div className="ingredients-div" style={{ display: "flex" }}>
-      <div>
-        <h1>포함</h1>
-        <div className="item-div">
-          {include.map((item, idx) => (
-            <span
-              key={idx}
-              onClick={() => {
-                setInclude((prev) => prev.filter((el) => el !== item));
-              }}
-            >
-              {item}
-            </span>
-          ))}
-        </div>
-      </div>
+    return (
+        <div>
+            <div style={{ display: "flex" }}>
+                {include.map((item, idx) => (
+                    <Button
+                        key={idx}
+                        onClick={() => {
+                            setInclude((prev) =>
+                                prev.filter((el) => el !== item)
+                            );
+                        }}
+                        style={{ border: "2px solid black", height: "40px" }}
+                    >
+                        {item}
+                    </Button>
+                ))}
+                <p>는 포함된</p>
+            </div>
 
-      <div>
-        <h1>제외</h1>
-        <div className="item-div">
-          {exclude.map((item, idx) => (
-            <span
-              key={idx}
-              onClick={() => {
-                setExclude((prev) => prev.filter((el) => el !== item));
-              }}
-            >
-              {item}
-            </span>
-          ))}
+            <div>
+                <div style={{ display: "flex" }}>
+                    {exclude.map((item, idx) => (
+                        <Button
+                            key={idx}
+                            onClick={() => {
+                                setExclude((prev) =>
+                                    prev.filter((el) => el !== item)
+                                );
+                            }}
+                            style={{
+                                border: "2px solid black",
+                                height: "40px",
+                            }}
+                        >
+                            {item}
+                        </Button>
+                    ))}
+                    <p>는 제외된</p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
